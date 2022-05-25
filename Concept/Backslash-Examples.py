532df9b0f4e88aaaf2c3853a3ad89e195abc91d5
Lakksh Tyagi <88983800+Lakkshhh@users.noreply.github.com>
2022-05-25 16:08:02 +0530

    Create Backslash-Examples.py

---
+++ Concept/Backslash-Examples.py
@@ -0,0 +1,16 @@
+print("Hi my name is Lakksh and my fav letter is \n8")
+
+# If you want to include the /n part or whatever as output, use the commands below.
+
+print(r"Hi my name is Lakksh and my fav letter is \n 8")    # Used the 'r' method as you can see in the starting 
+print("Hi my name is Lakksh and my fav letter is \\n 8")
+print("He said \"Wow you look amazing!!\"")
+
+# So basically, the backslash function is used to ignore what is in front of it.
+
+# \\\ Extra Information ///
+
+today = "friday"              # in operator is used to check if the second value contains the first value
+print("day" in today)         # True
+print("fri" in today)         # True
+print("lake" in "lakksh")     # False
